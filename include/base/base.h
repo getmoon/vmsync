44e113242f4c6afd685b83d76724bdd8a4c3ea5f
root <root@localhost.localdomain>
2012-07-08 01:45:59 +0800

    library OK

--- include/base/base.h
+++ include/base/base.h
@@ -1,9 +1,30 @@
 #ifndef __BASE_H__
 #define __BASE_H__
 
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+
 /*
  *
  */
+
+#define KB(n) (1UL << 10)
+#define MB(n) (1UL << 20)
+#define GB(n) (1UL << 30)
+
+extern int vmsync_file_create(const char * file_name);
+extern int vmsync_file_remove(const char * file_name);
+
+extern int vmsync_flock_set(int fd, int code);
+
+#define vmsync_file_lock(fd) vmsync_flock_set(fd, F_WRLCK)
+#define vmsync_file_unlock(fd) vmsync_flock_set(fd, F_UNLCK)
 
 #endif
 
--- include/sync/fsync.h
+++ include/sync/fsync.h
@@ -1,14 +1,26 @@
 /*
  * Sync APIs - Any problem pls let me known. 
  * E-mail: chengtao786@gmail.com
+ * /tmp/sync/source: vmsync_init(), filename_fileid_blocksize
+ * /tmp/sync/send: vmsync_send(), filename_fileid_blocksize_blockid
+ * /tmp/sync/fail: vmsync_send() function fails
  */
 
 #ifndef __FSYNC_H__
 #define __FSYNC_H__
 
+#define SYNC_WORK_DIR "/tmp/sync/"
+
+enum {
+	ERROR_SYNC_OK = 0,
+	ERROR_SYNC_FNAMELEN ,
+	ERROR_SYNC_FBLOCKLEN ,
+	ERROR_SYNC_FID ,
+	ERROR_SYNC_INIT ,
+};
+
 extern int vmsync_init(const char * source_file_full_name, int file_id, uint32_t block_size);
 extern void vmsync_fini(const char * source_file_full_name, int file_id);
-
 extern int vmsync_send(int fild_id, uint64_t offset , uint64_t len);
 
 #endif
